ddb3b61583139d6f6cac67ab33fefc0385ddc07b
malu <medeiroslopesmarialuisa@gmail.com>
2026-04-13 18:16:16 -0300

    aulas

---
+++ aula1.py
@@ -0,0 +1,35 @@
+print("ola mundo")
+# escreve ola mundo
+nome = "batatinha"
+print(nome)
+
+nome = input("digite seu nome")
+idade = input("digite sua idade")
+print(nome , idade)
+print(nome )
+
+n1 = float(input("digite um numero"))
+n2 = float(input("digite um segundo numero"))
+
+'''soma = (n1 + n2)
+subtracao = -
+multiplicacao = *
+divisao = /
+potencia = **
+RestoDaDivisao= %'''
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+potencia = n1 ** n2
+
+print(soma, subtracao, multiplicacao, divisao, potencia)
+
+print(f"o resultado da soma: {soma} o resultado ")
+
+
+
+
+
+
---
+++ aula2.py
@@ -0,0 +1,31 @@
+
+convidados= ["giovani","guilherme","joao","ana","enzo"]
+#esquenta= convidados.pop(1) (remove e armazena o ultimo valor caso estaja em branco ou a posição selecionada)
+#convidados.remove(viajando)
+#                0          1         2     3      4
+#               -5         -4        -3    -2     -1 
+#print(convidados) (printa a lista toda)
+#print(convidados[1]) (printa determinada posição)
+#print(convidados[-1]) (printa o ultimo convidado)
+#convidados[0]= "bia"  (substitui determinada posição)
+#convidados.append("julia")  (coloca valor no final da lista)
+#convidados.insert(0,"ronan")  (insere na posição informada)
+#del convidados[3] (deleta da lista)
+
+#convidados.sort()   (altera a estrutura da lista e deixa ordenada{alfabetica,crescente})
+#convidados.sort(reverse=True)   (altera a estrutura da lista e deixa ordenada{alfabetica, decrescente} ou inverte a lista ordenada)
+#rint(sorted(convidados))   (apenas organiza e PRINTA, não altera a estrutura original)
+#rint(sorted(convidados, reverse=True))  (printa a lista invertida)
+
+#numeros=list(range(1,10)) ( gera uma lista de numeros)
+#print(min(numeros)) (menor numero da lista)
+#print(max(numeros)) (maior numero da lista)
+#print(len(numeros)) (retoma o tamanho da lista, tanto numerica quanto string)
+
+materiasxnotas=[["port","mat","geo", "hist"], [4,5,6,7]]
+print(materiasxnotas[1][3])
+print(materiasxnotas[0][0], materiasxnotas[1][0])
+
+print(convidados)
+print(esquenta)
+print(viajando)
